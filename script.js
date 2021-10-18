d81f8d5c0d778b4b90ad8c8de7b3155c49edb40a
hannarosenfeld <hannarosenfeld@gmail.com>
2021-10-18 12:55:57 -0400

    Move js into seperate file.

--- script.js
+++ script.js
@@ -1,54 +1,116 @@
-$( document ).ready(function() {
-    let minutes = 25
-    let seconds = '00'
-    let sessionLength = 25
-    let breakLength = 5
-    let minutes_interval = setInterval(minutesTimer, 60000)
-    let seconds_interval = setInterval(secondsTimer, 1000)
+var minutes
+var seconds
+var sessionLength
+var breakLength
+var minutesInterval
+var secondsInterval
+var timerIsRunning = null
 
-    const init = () => {
-        setDefaults()
+const init = () => {
+    setDefaults()
+}
+
+function pad(val) {
+    return ('00' + val).slice(-2)
+}
+
+function setDefaults() {
+    minutes = 25
+    seconds = '00'
+    breakLength = 5
+    sessionLength = 25
+
+    document.getElementById('time-left').innerHTML = minutes + ':' + seconds
+    document.getElementById('session-length').innerHTML = sessionLength
+    document.getElementById('break-length').innerHTML = breakLength
+}
+
+function decrementSessionLength() {
+    if (sessionLength >=2) {
+        sessionLength -= 1
+        document.getElementById('time-left').innerHTML = pad(sessionLength) + ':' + seconds
+        document.getElementById('session-length').innerHTML = sessionLength
     }
-    window.onload = init
-    
-    function pad(val) {
-        return ('00' + val).slice(-2)
+}
+
+function incrementSessionLength() {
+    if (sessionLength <= 59) {
+        sessionLength += 1
+        document.getElementById('time-left').innerHTML = pad(sessionLength) + ':' + seconds
+        document.getElementById('session-length').innerHTML = sessionLength
+    }
+}
+
+function decrementBreakLength() {
+    if (breakLength !== 1) {
+        breakLength -= 1
+        document.getElementById('break-length').innerHTML = breakLength
+    }
+}
+
+function incrementBreakLength() {
+    if (breakLength < 60) {
+        breakLength += 1
+        document.getElementById('break-length').innerHTML = breakLength
+    }
+}
+
+function resetSession(){
+    clearSessionInterval()
+    setDefaults()
+}
+
+function clearSessionInterval(){
+    clearInterval(minutesInterval)
+    clearInterval(secondsInterval)
+}
+
+function breakDown() {
+    breakLength = pad(breakLength - 1)
+    seconds = 59
+    document.getElementById('time-left').innerHTML = breakLength + ':' + '00'
+    minutesInterval = setInterval(minutesTimer, 60000)
+    secondsInterval = setInterval(secondsTimer, 1000)
+    function minutesTimer(){
+        breakLength > 1 ? breakLength -= 1 : breakLength = breakLength
+        document.getElementById('time-left').innerHTML = pad(breakLength) + ':' + pad(seconds)
+    }
+    function secondsTimer(){
+        seconds -= 1
+        document.getElementById('time-left').innerHTML = pad(breakLength) + ':' + pad(seconds)
+        if (seconds <= 0){
+            if (breakLength <= 0) {
+                clearSessionInterval()
+                document.getElementById('beep').play()
+            }
+            seconds = 60
+        }
     }
 
-    function setDefaults() {
-        let timeLeft = document.getElementById('time-left')
-        let breakLength = document.getElementById('break-length')
-        let sessionLength = document.getElementById('session-length')
+}
 
-        timeLeft.innerHTML = minutes + ':' + seconds
-        breakLength.innerHTML = 5
-        sessionLength.innerHTML = minutes
-    }
-
-    $('start').click(function(){
-        minutesTimer()
-        secondsTimer()
-    })
-    
-    const minutesTimer = () => {
+function countDown() {
+    minutes = pad(sessionLength - 1)
+    seconds = 59
+    document.getElementById('time-left').innerHTML = minutes + ':' + seconds
+    minutesInterval = setInterval(minutesTimer, 60000)
+    secondsInterval = setInterval(secondsTimer, 1000)
+    function minutesTimer(){
         minutes -= 1
-        document.getElementById('time-left').innerHTML = minutes + ':' + seconds
+        document.getElementById('time-left').innerHTML = pad(minutes) + ':' + pad(seconds)
     }
-    
-    const secondsTimer = () => {
+    function secondsTimer(){
         seconds -= 1
         document.getElementById('time-left').innerHTML = pad(minutes) + ':' + pad(seconds)
         if (seconds <= 0){
             if (minutes <= 0) {
-                clearInterval(minutes_interval)
-                clearInterval(seconds_interval)
+                clearSessionInterval()
                 document.getElementById('beep').play()
                 breakDown()
             }
             seconds = 60
         }
     }
+}
 
-    
-    console.log( "ready!" );
-});
+window.onload = init         
